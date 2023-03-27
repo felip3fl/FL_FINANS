dd5c8011b457eb59269d610668a73c203a013283
Felip3FL <felip3.fl@gmail.com>
2023-03-27 18:02:03 -0300

    New BFF project

---
+++ Source/FL.Finans.Bff/Controllers/Base/BaseController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FL.Finans.Bff.Controllers.Base
+{
+    public class BaseController : Controller
+    {
+
+        private HttpClient _httpClient;
+        private readonly string _urlBase;
+
+        public BaseController()
+        {
+            _urlBase = "";
+            _httpClient = new HttpClient();
+        }
+
+        protected async Task<T> Get<T>(String endpoint)
+        {
+            var response = await Get(endpoint);
+
+            return default;
+        }
+
+        private async Task<HttpResponseMessage> Get(string endpoint)
+        {
+            var output = await _httpClient.GetAsync(_urlBase + endpoint);
+
+            return output;
+        }
+
+    }
+}
---
+++ Source/FL.Finans.Bff/Controllers/V1/FinancialTransactionController.cs
@@ -0,0 +1,13 @@
+﻿using FL.Finans.Bff.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FL.Finans.Bff.Controllers.V1
+{
+    public class FinancialTransactionController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
