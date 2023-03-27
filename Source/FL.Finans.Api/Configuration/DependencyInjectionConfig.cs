f3a559d93234379bf52fe7eb504f49155c1a7473
Felip3FL <felip3.fl@gmail.com>
2023-03-27 17:07:04 -0300

    Dependency Injection (DI)

---
+++ Source/FL.Finans.Api/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,17 @@
+﻿using FL.Data.Inferfaces;
+using FL.Data.Repositories;
+
+namespace FL.Finans.Api.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            #region INFRA
+            services.AddScoped<IFinancialTransactionRepository, FinancialTransactionRepository>();
+            #endregion
+
+            return services;
+        }
+    }
+}
