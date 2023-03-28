162bfe355fe40111c745976fb2a09930e9c8f5bf
Felip3FL <felip3.fl@gmail.com>
2023-03-28 17:59:27 -0300

    Update Bff

--- Source/FL.Finans.Bff/Controllers/Base/BaseController.cs
+++ Source/FL.Finans.Bff/Controllers/Base/BaseController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json.Serialization;
 
 namespace FL.Finans.Bff.Controllers.Base
 {
@@ -14,6 +17,12 @@ namespace FL.Finans.Bff.Controllers.Base
             _httpClient = new HttpClient();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="endpoint"></param>
+        /// <returns></returns>
         protected async Task<T> Get<T>(String endpoint)
         {
             var response = await Get(endpoint);
@@ -21,11 +30,49 @@ namespace FL.Finans.Bff.Controllers.Base
             return default;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="endpoint"></param>
+        /// <returns></returns>
         private async Task<HttpResponseMessage> Get(string endpoint)
         {
             var output = await _httpClient.GetAsync(_urlBase + endpoint);
 
             return output;
+        }
+
+        protected async Task<T> PostAsync<T>(string endpoint, object objectSerialized, List<HttpClientParameter> header = null)
+        {
+            var response = await PostAsync(endpoint, JsonConvert.SerializeObject(objectSerialized), header);
+
+            if (response == null)
+            {
+                var contentResult = await 
+            }
+        }
+
+        private async Task<HttpResponseMessage> PostAsync(string endpoint, string objectSerialized, List<HttpClientParameter> header)
+        {
+            var request = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                Content = new StringContent(objectSerialized, Encoding.UTF8, "application/json"),
+                RequestUri = new Uri(endpoint),
+            };
+
+            var client = new HttpClient();
+            client.Timeout = TimeSpan.FromMicroseconds(60000);
+
+            if (header == null)
+                header = new List<HttpClientParameter>();
+
+            for(int i = 0; i < header.Count; i++)
+                request.Headers.Add(header[i].Name, header[i].Value);
+            
+            var result = await client.SendAsync(request);
+
+            return result;
         }
 
     }
---
+++ Source/FL.Model/HttpClientParameter.cs
@@ -0,0 +1,8 @@
+﻿namespace FL.Model
+{
+    public class HttpClientParameter
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
