e22dc719c8b49df006582c625c2c3cffd27ef801
Felip3FL <felip3.fl@gmail.com>
2023-03-29 18:05:50 -0300

    Update baseController

--- Source/FL.Finans.Bff/Controllers/Base/BaseController.cs
+++ Source/FL.Finans.Bff/Controllers/Base/BaseController.cs
@@ -42,16 +42,67 @@ namespace FL.Finans.Bff.Controllers.Base
             return output;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="endpoint"></param>
+        /// <param name="objectSerialized"></param>
+        /// <param name="header"></param>
+        /// <returns></returns>
         protected async Task<T> PostAsync<T>(string endpoint, object objectSerialized, List<HttpClientParameter> header = null)
         {
             var response = await PostAsync(endpoint, JsonConvert.SerializeObject(objectSerialized), header);
 
             if (response == null)
             {
-                var contentResult = await 
+                var contentResult = await response.Content.ReadAsStringAsync();
+                var output = JsonConvert.DeserializeObject<T>(contentResult);
+                return output;
             }
+
+            return default;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="endpoint"></param>
+        /// <returns></returns>
+        protected async Task<HttpResponseMessage> DeleteById(string endpoint)
+        {
+            return await _httpClient.DeleteAsync(_urlBase + endpoint);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="endpoint"></param>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        protected async Task<HttpResponseMessage> PutAsync(string endpoint, object input)
+        {
+            var objectSerialized = input == null ? String.Empty : JsonConvert.SerializeObject(input);
+
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Put,
+                Content = new StringContent(objectSerialized, Encoding.UTF8, "application/json"),
+                RequestUri = new Uri(_urlBase + endpoint),
+            };
+
+            var output = await _httpClient.SendAsync(request);
+
+            return output;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="endpoint"></param>
+        /// <param name="objectSerialized"></param>
+        /// <param name="header"></param>
+        /// <returns></returns>
         private async Task<HttpResponseMessage> PostAsync(string endpoint, string objectSerialized, List<HttpClientParameter> header)
         {
             var request = new HttpRequestMessage()
