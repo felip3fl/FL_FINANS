94b50ae9dfceb4fcb759ab33062f4fc8858ece8a
Felip3FL <felip3.fl@gmail.com>
2023-04-15 09:05:54 -0300

    Moved Swagger and ApiConfig to another class

---
+++ Source/FL.Finans.Api/Configuration/ApiConfig.cs
@@ -0,0 +1,27 @@
+﻿namespace FL.Finans.Api.Configuration
+{
+    public static class ApiConfig
+    {
+        public static IServiceCollection AddApiConfiguration(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddControllers();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            return app; 
+        }
+    }
+}
---
+++ Source/FL.Finans.Api/Configuration/SwaggerConfig.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace FL.Finans.Api.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "FL Enterprise API",
+                    Description = "Esta API faz parte do futuro projeto",
+                    Contact = new OpenApiContact() { Name = "Felipe Lima", Email = "felip3.fl@gmail.com" },
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri(uriString: "https://opensource.org/1icenses/MIT") }
+                });
+
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            return app;
+        }
+    }
+}
