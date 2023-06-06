c79b355b24c5d9b77ec6324ed739a31cb8a74c7d
Felip3FL <felip3.fl@gmail.com>
2023-06-05 21:41:20 -0300

    Polly

--- Source/FL.Point.Bff/Configuration/DependencyInjectionConfig.cs
+++ Source/FL.Point.Bff/Configuration/DependencyInjectionConfig.cs
@@ -1,5 +1,8 @@
 ﻿using FL.Point.Bff.Services;
 using FL.Point.Bff.Services.Interface;
+using Polly;
+using Polly.Extensions.Http;
+using Polly.Retry;
 
 namespace FL.Point.Bff.Configuration
 {
@@ -7,9 +10,35 @@ namespace FL.Point.Bff.Configuration
     {
         public static IServiceCollection ResolveDependencies(this IServiceCollection services)
         {
-            services.AddHttpClient<IEletronicPointService, EletronicPointService>();
+            services.AddHttpClient<IEletronicPointService, EletronicPointService>()
+            .AddTransientHttpErrorPolicy(
+            p => p.WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(10000)));
+            //.AddPolicyHandler(PollyExtensions.EsperarTentar());
+            //TODO Polly não está funcionando
 
             return services;
+        }
+    }
+
+    public class PollyExtensions
+    {
+        public static AsyncRetryPolicy<HttpResponseMessage> EsperarTentar()
+        {
+            var retry = HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(new[]
+                {
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(2),
+                    TimeSpan.FromSeconds(4),
+                }, (outcome, timespan, retryCount, context) =>
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"Tentando pela {retryCount} vez!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                });
+
+            return retry;
         }
     }
 }
