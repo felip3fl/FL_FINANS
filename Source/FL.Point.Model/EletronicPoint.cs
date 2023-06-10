caf60a8dc99af7f1b6bdb614739b95e0cec71dd0
Felip3FL <felip3.fl@gmail.com>
2023-06-10 14:47:45 -0300

    Update

--- Source/FL.Point.Model/EletronicPoint.cs
+++ Source/FL.Point.Model/EletronicPoint.cs
@@ -7,5 +7,16 @@ namespace FL.Point.Model
     {
         public DateTime Date { get; set; }
         public EletronicPointType Type { get; set; }
+
+        //EF Relation
+        protected EletronicPoint()
+        { 
+        }
+            
+        //public EletronicPoint(DateTime date, EletronicPointType type)
+        //{
+        //    Date = date;
+        //    Type = new EletronicPointType(type);
+        //}
     }
 }
--- Source/FL.Point.Model/EletronicPointType.cs
+++ Source/FL.Point.Model/EletronicPointType.cs
@@ -5,5 +5,16 @@ namespace FL.Point.Model
     public class EletronicPointType : BaseModel
     {
         public string Name { get; set; }
+
+        //Contrutor do EF
+        protected EletronicPointType()
+        {
+
+        }
+
+        public EletronicPointType(string name)
+        {
+            name = Name;
+        }
     }
 }
