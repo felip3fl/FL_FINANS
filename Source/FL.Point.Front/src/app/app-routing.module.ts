92f5fea7bd34ab54a6e2dfbb8302ce7b82b30b2b
Felip3FL <felip3.fl@gmail.com>
2023-07-07 11:06:36 -0300

    Add DetailsComponent on router

--- Source/FL.Point.Front/src/app/app-routing.module.ts
+++ Source/FL.Point.Front/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './navigation/home/home.component';
-import { Details } from './eletronicPoint/details/details.component';
+import { DetailsComponent } from './eletronicPoint/details/details.component';
 
 const routes: Routes = [
     { path: '',  component: HomeComponent, pathMatch: 'full' },
-    { path: 'detail',  component: Details, pathMatch: 'full' },
+    { path: 'detail',  component: DetailsComponent },
   ];
   
   @NgModule({
