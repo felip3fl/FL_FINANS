f8604180fd52ff86bd338c68e695a8c784ad4110
Felip3FL <felip3.fl@gmail.com>
2023-07-25 22:16:37 -0300

    Update EletronicPoint

--- Source/FL.Point.Api/Controllers/V1/EletronicPointController.cs
+++ Source/FL.Point.Api/Controllers/V1/EletronicPointController.cs
@@ -50,6 +50,78 @@ namespace FL.Point.Api.Controllers.V1
             return Ok(result);
         }
 
+        ///// <summary>
+        ///// Get all Financial Transaction
+        ///// </summary>
+        ///// <returns></returns>
+        //[HttpGet("GetAllFinancialTransactions")]
+        //[ProducesResponseType(typeof(List<FinancialTransaction>), StatusCodes.Status200OK)]
+        //[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        //public async Task<ActionResult<List<FinancialTransaction>>> GetAll()
+        //{
+        //    return await _financialTransactionRepository.GetAll();
+        //}
+
+        ///// <summary>
+        ///// Get a Financial Transaction by Id
+        ///// </summary>
+        ///// <param name="id"></param>
+        ///// <returns></returns>
+        //[HttpGet("GetFinancialTransaction/{id:int}")]
+        //[ProducesResponseType(typeof(FinancialTransaction), StatusCodes.Status200OK)]
+        //[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        //public async Task<ActionResult<FinancialTransaction>> Get(int id)
+        //{
+        //    return await _financialTransactionRepository.GetById(id);
+        //}
+
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        ///// <param name="financialTransaction"></param>
+        ///// <returns></returns>
+        //[HttpPut]
+        //public async Task<ActionResult<List<String>>> Put(FinancialTransaction financialTransaction)
+        //{
+        //    var transaction = new FinancialTransaction();
+
+        //    if (transaction == null)
+        //        AdicionarErroProcessamento("Erro - Objeto vazio");
+
+        //    return CustomResponse();
+        //}
+
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        ///// <param name="financialTransaction"></param>
+        ///// <returns></returns>
+        //[HttpDelete]
+        //public async Task<ActionResult<List<String>>> Delete(FinancialTransaction financialTransaction)
+        //{
+        //    var transaction = new FinancialTransaction();
+
+        //    if (transaction == null)
+        //        AdicionarErroProcessamento("Erro - Objeto vazio");
+
+        //    return CustomResponse();
+        //}
+
+
+        ///// <summary>
+        ///// Save the transaction
+        ///// </summary>
+        ///// <param name="financialTransaction"></param>
+        ///// <returns></returns>
+        //[HttpPost]
+        //public async Task<ActionResult> Update(FinancialTransaction financialTransaction)
+        //{
+        //    if (financialTransaction.Id == "")
+        //        AdicionarErroProcessamento("Erro - Objeto financialTransaction vazio");
+
+        //    return CustomResponse(); ;
+        //}
+
         #endregion
     }
 }
