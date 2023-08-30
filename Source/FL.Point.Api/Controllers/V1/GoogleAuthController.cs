97dbc70f64a498896357340ad21a1073a63f1d18
Felip3FL <felip3.fl@gmail.com>
2023-08-30 11:30:38 -0300

    New Auth Controller

---
+++ Source/FL.Point.Api/Controllers/V1/GoogleAuthController.cs
@@ -0,0 +1,52 @@
+﻿using FL.Point.GoogleCalendarApi.Interfaces;
+using FL.Point.Model;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FL.Point.Api.Controllers.V1
+{
+    public class GoogleAuthController : Controller
+    {
+
+        private IGoogleCalendarService _googleCalendarService;
+        public GoogleAuthController(IGoogleCalendarService googleCalendarService)
+
+        {
+            _googleCalendarService = googleCalendarService;
+        }
+
+        [HttpGet]
+        [Route("/user/index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/auth/google")]
+        public async Task<IActionResult> GoogleAuth()
+        {
+            return Redirect(_googleCalendarService.GetAuthCode());
+        }
+
+        [HttpGet]
+        [Route("/auth/callback")]
+        public async Task<IActionResult> Callback()
+        {
+            string code = HttpContext.Request.Query["code"];
+            string scope = HttpContext.Request.Query["scope"];
+
+            //get token method
+            var token = await _googleCalendarService.GetTokens(code);
+            return Ok(token);
+        }
+
+        [HttpPost]
+        [Route("/user/calendarevent")]
+        public async Task<IActionResult> AddCalendarEvent([FromBody] GoogleCalendarReqDTO calendarEventReqDTO)
+        {
+            var data = _googleCalendarService.AddToGoogleCalendar(calendarEventReqDTO);
+            return Ok(data);
+        }
+    }
+}
