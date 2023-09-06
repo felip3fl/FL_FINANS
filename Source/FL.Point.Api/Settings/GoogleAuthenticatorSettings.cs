7c4c2592f9f06453ba672fac3dc7d9ea2344ffb7
Felip3FL <felip3.fl@gmail.com>
2023-09-06 13:08:18 -0300

    Update GoogleAuthenticator proprietary

--- Source/FL.Point.Api/Settings/GoogleAuthenticatorSettings.cs
+++ Source/FL.Point.Api/Settings/GoogleAuthenticatorSettings.cs
@@ -2,8 +2,7 @@
 {
     public class GoogleAuthenticatorSettings
     {
-        public string Url { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public string ClientID { get; set; }
+        public string ClientSecret { get; set; }
     }
 }
