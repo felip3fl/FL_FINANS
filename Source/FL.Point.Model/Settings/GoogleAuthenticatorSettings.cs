40eb67e310b42a0efcae92b5e12cf99fe9109e5a
Felip3FL <felip3.fl@gmail.com>
2023-10-02 09:13:15 -0300

    New model GoogleAuthenticatorSettings

---
+++ Source/FL.Point.Model/Settings/GoogleAuthenticatorSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace FL.Point.Model.Settings
+{
+    public class GoogleAuthenticatorSettings
+    {
+        public string ClientID { get; set; }
+        public string ClientSecret { get; set; }
+        public string UnreservedChars { get; set; }
+    }
+}
