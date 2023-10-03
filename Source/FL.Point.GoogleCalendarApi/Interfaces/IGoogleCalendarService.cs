26b6a728b038bd66f2a7a959867da8e8fdea5385
Felip3FL <felip3.fl@gmail.com>
2023-10-03 17:46:03 -0300

    Improve IGoogleCalendarService

--- Source/FL.Point.GoogleCalendarApi/Interfaces/IGoogleCalendarService.cs
+++ Source/FL.Point.GoogleCalendarApi/Interfaces/IGoogleCalendarService.cs
@@ -10,7 +10,6 @@ namespace FL.Point.GoogleCalendarApi.Interfaces
     public interface IGoogleCalendarService
     {
         string GetAuthCode(string clientID, string unreservedChars);
-
         Task<GoogleTokenResponse> GetTokens(string code, string clientID, string clientSecret);
         Task<GoogleTokenResponse> GetAndSaveTokens(string code, string clientID, string clientSecret);
         string AddToGoogleCalendar(GoogleCalendarReqDTO googleCalendarReqDTO);
