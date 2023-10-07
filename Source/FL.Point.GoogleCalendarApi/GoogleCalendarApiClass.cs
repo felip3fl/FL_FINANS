b732621fb650fe5e7f20d7a5688e5190c3cd1fad
Felip3FL <felip3.fl@gmail.com>
2023-10-07 16:12:39 -0300

    Saved code from GoogleCalendarApi

---
+++ Source/FL.Point.GoogleCalendarApi/GoogleCalendarApiClass.cs
@@ -0,0 +1,31 @@
+﻿using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+using System.Xml.Xsl;
+
+namespace FL.Point.GoogleCalendarApi
+{
+    public class GoogleCalendarApiClass
+    {
+        const string ApiKey = "";
+        const string CalendarId = "";
+
+        public async void ListAll()
+        {
+
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                ApiKey = ApiKey,
+                ApplicationName = "Teste"
+            });
+
+            var request = service.Events.List(CalendarId);
+            request.Fields = "items(summary, start, end)";
+            var responde = await request.ExecuteAsync();
+
+            foreach(var item in responde.Items)
+            {
+                Console.WriteLine($"Aqui: {item.Summary}");
+            }
+        }
+    }
+}
