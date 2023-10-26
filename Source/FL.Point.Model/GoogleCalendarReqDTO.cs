8e29eaf16d5f6afb3891abae28b3d3d2aba204d0
Felip3FL <felip3.fl@gmail.com>
2023-10-25 22:17:26 -0300

    Removed unused 'usings'

--- Source/FL.Point.Model/GoogleCalendarReqDTO.cs
+++ Source/FL.Point.Model/GoogleCalendarReqDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FL.Point.Model
+﻿namespace FL.Point.Model
 {
     public class GoogleCalendarReqDTO
     {
