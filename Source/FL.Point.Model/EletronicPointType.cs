c8d15be7ffa939df9e3524cdb7aad5ccf6702f63
FL <felip3.fl@gmail.com>
2024-03-04 09:18:39 -0300

    Removed comments

--- Source/FL.Point.Model/EletronicPointType.cs
+++ Source/FL.Point.Model/EletronicPointType.cs
@@ -5,16 +5,5 @@ namespace FL.Point.Model
     public class EletronicPointType : BaseModel
     {
         public string Name { get; set; }
-
-        //Contrutor do EF
-        //protected EletronicPointType()
-        //{
-
-        //}
-
-        //public EletronicPointType(string name)
-        //{
-        //    name = Name;
-        //}
     }
 }
